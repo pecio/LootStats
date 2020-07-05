19829656c2454c7faff4c98693cb4d4419c1000b
Raul Pedroche Novillo <r.pedroche.novillo@gmail.com>
2020-07-05 07:44:22 +0200

    Add deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,15 @@
-#!/bin/bash
-WOW="/Applications/World of Warcraft"
-ADDON="$(basename "$PWD")"
-/usr/bin/rsync -av . --exclude=.git "${WOW}/Interface/AddOns/${ADDON}/"
+#! /bin/sh
+PROJECT=LootStats
+if [ -z "${WOW_HOME}" ]
+then
+  WOW_HOME="/mnt/c/Program files (x86)/World of Warcraft/_retail_"
+fi
+if [ ! -f "${PROJECT}.toc" ]
+then
+  echo "Run from project directory" >&2
+  exit 1
+fi
+/usr/bin/rsync -av --exclude .git \
+	--exclude .gitignore \
+	--exclude deploy.sh \
+	./ "${WOW_HOME}/Interface/AddOns/${PROJECT}/"
